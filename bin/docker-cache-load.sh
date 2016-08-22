7431df29cdeae14972c03bf79b4e68aa9e6dcc87
Giorgos Logiotatidis <seadog@sealabs.net>
2016-08-22 10:48:55 +0300

    foo

--- bin/docker-cache-load.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for CACHE in `ls ${1}`;
-do
-    gunzip -c ${1}/${CACHE} | docker load
-done
--- bin/docker-cache-save.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LAYERS=`docker history -q ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} | tail -n +2 | grep -v '<missing>'`
-
-for LAYER in $LAYERS;
-do
-    docker save ${LAYER} | gzip > ${1}/${LAYER}.tar.gz
-done
